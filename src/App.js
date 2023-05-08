693cc042ecd9074423ed5748a8f1514f8fb14a77
Shaik Ahmad Nawaz <96189881+shaikahmadnawaz@users.noreply.github.com>
2023-05-08 06:06:14 +0000

    testing how codespaces are working

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ function App() {
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
       </header>
     </div>
   );
